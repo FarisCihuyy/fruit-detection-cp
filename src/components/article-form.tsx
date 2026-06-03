b786b93e530689419b48a90d6b1e01fcdde76c25
FarisCihuyy <farisngab78@gmail.com>
2026-06-03 19:26:44 +0700

    fix article form

--- src/components/article-form.tsx
+++ src/components/article-form.tsx
@@ -155,7 +155,7 @@ export function ArticleForm({
                 {...field}
                 id="description"
                 placeholder="Description..."
-                className="flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                className="flex min-h-20 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               />
 
               {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
@@ -166,8 +166,7 @@ export function ArticleForm({
         <Controller
           name="thumbnail"
           control={form.control}
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          render={({ a, fieldState }) => (
+          render={({ fieldState }) => (
             <Field data-invalid={fieldState.invalid}>
               <FieldLabel htmlFor="thumbnail">Thumbnail (PNG/JPG)</FieldLabel>
 
